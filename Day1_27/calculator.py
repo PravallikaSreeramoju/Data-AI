51b5fe7cbafb7a38338550210aa8ea2ea30374d8
S Lakshmi Pravallika <sreeramoju.pravallika@gmail.com>
2026-01-28 15:33:16 +0530

    Add files via upload

---
+++ Day1_27/calculator.py
@@ -0,0 +1,29 @@
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def mod(a,b):
+    return a%b
+
+a=int(input())
+op=str(input())
+b=int(input())
+
+if(op== '+'):
+    print(add(a,b))
+elif(op=='-'):
+    print(sub(a,b))
+elif(op=='*'):
+    print(mul(a,b))
+elif(op=='/'):
+    print(div(a,b))
+elif(op=='%'):
+    print(mod(a,b))
